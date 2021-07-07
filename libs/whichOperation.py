2a734430d67122e91cdc33c467d036d77b8d4b5c
Caleb <calebcipra@gmail.com>
2021-07-07 11:39:54 -0400

    better checkbox position

--- libs/whichOperation.py
+++ libs/whichOperation.py
@@ -1,7 +1,5 @@
 def whichOperation(myCell, eachOperation, myValue, tipo):
-    # print("asdasd333333")
     if (tipo == "common"):
-        # print("common")
         if (eachOperation.find("==") > -1):
             if (str(myCell) == str(myValue)):
                 return True
@@ -10,55 +8,18 @@ def whichOperation(myCell, eachOperation, myValue, tipo):
         else:
             return eval(str(myCell) + eachOperation + str(myValue))
 
-        # if (eachOperation.find("==") > -1):
-        #     if (str(myCell) == str(myValue)):
-        #         return True
-        #     else:
-        #         return False
-        # elif (eachOperation.find(">=") > -1):
-        #     if (myCell >= myValue):
-        #         return True
-        #     else:
-        #         return False
-        # elif (eachOperation.find("<=") > -1):
-        #     if (myCell <= myValue):
-        #         return True
-        #     else:
-        #         return False
-        # elif(eachOperation.find("!=") > -1):
-        #     if (myCell != myValue):
-        #         return True
-        #     else:
-        #         return False
-        # elif(eachOperation.find(">") > -1):
-        #     if (myCell > myValue):
-        #         return True
-        #     else:
-        #         return False
-        # elif(eachOperation.find("<") > -1):
-        #     if (myCell < myValue):
-        #         return True
-        #     else:
-        #         return False
     elif (tipo == "re"):
-        # print("Estamos en re")
         cant = 0
-        # print(eachOperation[1:])
         if(eachOperation.startswith("*") and eachOperation.endswith("*")):
             if (cant != 1):
                 cant = 1
-                # print("Here we are")
                 isIt = str(eachOperation)[1:-1] in str(myCell)
-                # print(isIt)
                 return isIt
         elif (eachOperation.startswith("*") and str(myCell).startswith(str(eachOperation[1:]))):
             if (cant != 1):
                 cant = 1
-                # print("start with")
                 return True
         elif (eachOperation.endswith("*") and str(myCell).endswith(str(eachOperation[:-1]))):
             if (cant != 1):
-                # print(str(myValue)[:-1])
                 cant = 1
-                # print("ends with")
                 return True
